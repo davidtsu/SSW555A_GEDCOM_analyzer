23d3c223927c2c180584c4effb95849281e97fff
David Tsu <davidtsu97@gmail.com>
2020-02-05 17:48:22 -0500

    file reorg, can read & print most data. todo: date

--- ssw555a_ged.py
+++ ssw555a_ged.py
@@ -8,112 +8,292 @@ from prettytable import PrettyTable
 
 class GED_Repo:
     """ stores data from a GEDCOM file """
-    def __init__(self):
+    def __init__(self, directory):
         """ constructor for GED_Repo, creates list of individuals and families """
+        self.directory = directory
         self.individuals = dict()
         self.families = dict()
+
+        try:
+            # this will analyze all files in the input_files directory
+            for file in os.listdir(os.path.join(self.directory, 'input_files')):
+                self.read_ged(os.path.join(self.directory, 'input_files', file))
+        except FileNotFoundError as f:
+            raise f
+        except ValueError as v:
+            raise v
+
+    def read_ged(self, ip, sep=' '):
+        """ For reading GEDCOM files """
+        tags = {
+            '0': {
+                'VALID': {'NOTE', 'HEAD', 'TRLR'},
+                'SWAP': {'INDI', 'FAM'}
+            },
+            '1': {
+                'VALID': {'NAME', 'SEX', 'FAMC', 'FAMS', 'HUSB', 'WIFE', 'CHIL', 'BIRT', 'DEAT', 'MARR', 'DIV'},
+                'SWAP': set()   # not sure I need to make this, but in case of future extension I'll leave it.
+            },
+            '2': {
+                'VALID': {'DATE'},
+                'SWAP': set()
+            }
+        }
+
+        try:
+            # opens input file
+            with open(ip, 'r') as fp_in:
+                ind = Individual()
+                i_flag = False
+                fam = Family()
+                f_flag = False
+
+                for line in non_blank_lines(fp_in):
+                    # split each line at spaces
+                    my_tuple = tuple(line.strip().split(sep, 2))
+
+                    # non_blank_lines generator removes blank lines, might not need generator with this check in place.
+                    if len(my_tuple) < 2:
+                        raise ValueError(f'Line in {ip} has too few values. Please fix and try again.')
+
+                    # renaming for sanity
+                    level = my_tuple[0]
+                    tag = my_tuple[1]
+                    arg = '' if len(my_tuple) == 2 else my_tuple[2]
+                    out_line = tuple()
+
+                    # level and tag checking
+                    if level in tags.keys():
+                        # if tag in dict
+                        if tag in tags[level]['VALID']:
+                            # add line value to corresponding family or individual dict. maybe use boolean flags?
+                            out_line = (level, tag, 'Y', arg)
+
+                            # check all individual tags here
+                            if i_flag:
+                                if tag == 'NAME':
+                                    ind.set_name(arg)
+                                elif tag == 'SEX':
+                                    ind.set_gender(arg)
+                                elif tag == 'FAMC':
+                                    ind.set_child(arg)
+                                elif tag == 'FAMS':
+                                    ind.set_spouse(arg)
+                                elif tag == 'BIRT':
+                                    # TODO: get date from next line
+                                    # ind.set_birthday(arg)
+                                    pass
+                                elif tag == 'DEAT':
+                                    # TODO: get date from next line
+                                    ind.set_alive(False)
+                                    # ind.set_death(arg)
+                                    pass
+
+                            # check all family tags here
+                            if f_flag:
+                                if tag == 'HUSB':
+                                    fam.set_husb_id(arg)
+                                    fam.set_husb_name(self.individuals[arg].name)
+                                elif tag == 'WIFE':
+                                    fam.set_wife_id(arg)
+                                    fam.set_wife_name(self.individuals[arg].name)
+                                elif tag == 'CHIL':
+                                    fam.set_children(arg)
+                                elif tag == 'MARR':
+                                    # TODO: need to get date from next line
+                                    # fam.set_married(DATE HERE)
+                                    pass
+                                elif tag == 'DIV':
+                                    # TODO: need to get date from next line
+                                    # fam.set_divorced(DATE HERE)
+                                    pass
+                                else:
+                                    print(f'tag={tag}')
+                                    # raise ValueError('How did we end up here?')
+
+                        elif arg in tags[level]['SWAP']:
+                            # new individual/family starts here
+                            # if old individual/family exists, save it to dictionary
+                            if ind.iid != '':
+                                ind = self.add_individual(ind)
+                                i_flag = f_flag = False
+                            if fam.fid != '':
+                                fam = self.add_family(fam)
+                                i_flag = f_flag = False
+
+                            # starting new ind/fam
+                            if arg == 'INDI':
+                                ind.set_iid(tag)
+                                i_flag = True
+                            elif arg == 'FAM':
+                                fam.set_fid(tag)
+                                f_flag = True
+
+                            out_line = (level, arg, 'Y', tag)
+                                
+                        # tag or arg not in dict
+                        else:
+                            out_line = (level, tag, 'N', arg)
+                    # invalid level
+                    else:
+                        out_line = (level, tag, 'N', arg)
+
+                    # print for clarity
+                    #print('--> ' + line + '\n')
+                    #print('<-- ' + '|'.join(out_line) + '\n')
+
+                # need to check once here for final individual/family item
+                if ind.iid != '':
+                    ind = self.add_individual(ind)
+                    i_flag = f_flag = False
+                if fam.fid != '':
+                    fam = self.add_family(fam)
+                    i_flag = f_flag = False
+
+        # raise error if bad files.
+        except ValueError as v:
+            raise ValueError(f'Bad value. Please check {ip} for bad data: {v}')
+        except FileNotFoundError:
+            raise FileNotFoundError(f'Cannot open file. Please check {ip} exists and try again.')
+
+    def add_individual(self, i):
+        """ must pass in individual """
+        self.individuals[i.iid] = i
+        return Individual()
+
+    def add_family(self, f):
+        """ must pass in family """
+        self.families[f.fid] = f
+        return Family()
 
     def print_individuals(self):
         """ prints list of individuals using prettytable """
         pt = PrettyTable()
         pt.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
-        #for i in self.individuals.values():
-        #    pt.add_row()
-        pass
+        for i in self.individuals.values():
+            pt.add_row(i.get_values())
+        print(pt)
 
     def print_families(self):
         """ prints list of families using prettytable """
         pt = PrettyTable()
         pt.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
-        pass
+        for f in self.families.values():
+            pt.add_row(f.get_values())
+        print(pt)
 
 class Individual:
     """ stores info for a single individual """
-    def __init__(self):
+    def __init__(self, iid = '', name = '', gender = '', birthday = '', age = 0, alive = True, death = 'NA', child = 'NA', spouse = 'NA'):
         """ constructor for Individual """
-        self.id = ''
-        self.name = ''
-        self.gender = ''
-        self.birthday = ''
-        self.age = 0
-        self.alive = True
-        self.death = ''
-        self.child = ''
-        self.spouse = ''
+        self.iid = iid
+        self.name = name
+        self.gender = gender
+        self.birthday = birthday
+        self.age = age
+        self.alive = alive
+        self.death = death
+        self.child = child
+        self.spouse = spouse
+
+    def get_values(self):
+        """ returns all values in individual as list for use in print """
+        return [self.iid, self.name, self.gender, self.birthday, self.age, self.alive, self.death, self.child, self.spouse]
+
+    def set_iid(self, i):
+        """ sets new individual id (iid) """
+        self.iid = i
+
+    def set_name(self, n):
+        """ sets new individual name """
+        self.name = n
+
+    def set_gender(self, g):
+        """ sets new individual gender """
+        self.gender = g
+    
+    def set_birthday(self, b):
+        """ sets new individual birthday """
+        self.birthday = b
+
+    def set_age(self, a):
+        """ sets new individual birthday """
+        self.age = a
+
+    def set_alive(self, a):
+        """ sets new individual living status """
+        self.alive = a
+
+    def set_death(self, d):
+        """ sets new individual death date """
+        self.death = d
+
+    def set_child(self, c):
+        """ adds child to individual's children """
+        if isinstance(self.child, set):
+            self.child = self.child | {c}
+        else:
+            self.child = {c} if (c and c != 'NA') else 'NA'
+    
+    def set_spouse(self, s):
+        """ sets new individual spouse """
+        if isinstance(self.spouse, set):
+            self.spouse = self.spouse | {s}
+        else:
+            self.spouse = {s} if (s and s != 'NA') else 'NA'
 
 class Family:
     """ stores info for a family """
-    def __init__(self):
+    def __init__(self, fid = '', married = '', divorced = 'NA', husb_id = '', husb_name = '', wife_id = '', wife_name = '', children = ''):
         """ constructor for family """
-        self.id = ''
-        self.married = ''
-        self.divorced = ''
-        self.husb_id = ''
-        self.husb_name = ''
-        self.wife_id = ''
-        self.wife_name = ''
-        self.children = ''
+        self.fid = fid
+        self.married = married
+        self.divorced = divorced
+        self.husb_id = husb_id
+        self.husb_name = husb_name
+        self.wife_id = wife_id
+        self.wife_name = wife_name
+        self.children = children
 
-def read_ged(ip, op, sep=','):
-    """ For reading GEDCOM files """
-    tags = {
-        '0': {
-            'VALID': {'NOTE', 'HEAD', 'TRLR'},
-            'SWAP': {'INDI', 'FAM'}
-        },
-        '1': {
-            'VALID': {'NAME', 'SEX', 'FAMC', 'FAMS', 'HUSB', 'WIFE', 'CHIL', 'BIRT', 'DEAT', 'MARR', 'DIV'},
-            'SWAP': set()   # not sure I need to make this, but in case of future extension I'll leave it.
-        },
-        '2': {
-            'VALID': {'DATE'},
-            'SWAP': set()
-        }
-    }
+    def get_values(self):
+        """ returns all values in family for use in print """
+        return [self.fid, self.married, self.divorced, self.husb_id, self.husb_name, self.wife_id, self.wife_name, self.children]
 
-    try:
-        # deletes old output file, if it exists
-        if os.path.exists(op):
-            os.remove(op)
+    def set_fid(self, i):
+        """ sets new family id """
+        self.fid = i
 
-        # opens input and output files
-        with open(ip, 'r') as fp_in, open(op, 'x') as fp_out:
-            for line in non_blank_lines(fp_in):
-                # split each line at spaces
-                my_tuple = tuple(line.strip().split(sep, 2))
+    def set_married(self, m):
+        """ sets new family marriage date """
+        self.married = m
 
-                # non_blank_lines generator removes blank lines, might not need generator with this check in place.
-                if len(my_tuple) < 2:
-                    raise ValueError(f'Line in {ip} has too few values. Please fix and try again.')
+    def set_divorced(self, d):
+        """ sets new family divorce date """
+        self.divorced = d
 
-                # renaming for sanity
-                level = my_tuple[0]
-                tag = my_tuple[1]
-                arg = '' if len(my_tuple) == 2 else my_tuple[2]
-                out_line = tuple()
+    def set_husb_id(self, h):
+        """ sets new family husb_id """
+        self.husb_id = h
 
-                # level and tag checking
-                if level in tags.keys():
-                    # if tag in dict
-                    if tag in tags[level]['VALID'] or arg in tags[level]['SWAP']:
-                        # TODO: ADD MORE ANALYSIS HERE. ALL VALID LINES SHOULD FALL IN HERE.
-                        out_line = (level, tag, 'Y', arg)
-                    # tag or arg not in dict
-                    else:
-                        out_line = (level, tag, 'N', arg)
-                # invalid level
-                else:
-                    out_line = (level, tag, 'N', arg)
+    def set_husb_name(self, h):
+        """ sets new family husb_name """
+        self.husb_name = h
 
-                # write input and output lines to output file
-                fp_out.write('--> ' + line + '\n')
-                fp_out.write('<-- ' + '|'.join(out_line) + '\n')
+    def set_wife_id(self, w):
+        """ sets new family wife_id """
+        self.wife_id = w
 
-    # raise error if bad files.
-    except ValueError as v:
-        raise ValueError(f'Bad value. Please check {ip} for bad data: {v}')
-    except FileNotFoundError:
-        raise FileNotFoundError(f'Cannot open files. Please check {ip} exists and try deleting {op} if it exists.')
+    def set_wife_name(self, w):
+        """ sets new family wife_name """
+        self.wife_name = w
+
+    def set_children(self, c):
+        """ adds child to family's children """
+        if isinstance(self.children, set):
+            self.children = self.children | {c}
+        else:
+            self.children = {c} if (c and c != 'NA') else 'NA'
+
 
 def non_blank_lines(f):
     """ generator to remove empty lines, found somewhere on Google """
@@ -126,22 +306,9 @@ def non_blank_lines(f):
 def main():
     """ for running GED reader. """
     # fetch user input
-    in_file = input("Enter input GED file name (no extension): ")
-
-    # file name manipulation
-    out_file = in_file + '_out'
-    in_file += '.ged'
-    out_file += '.ged'
-    ip = os.path.join(os.getcwd(), in_file)
-    op = os.path.join(os.getcwd(), out_file)
-
-    # read from ip, write to op
-    try:
-        read_ged(ip, op, ' ')
-    except ValueError as v:
-        print(v)
-    except FileNotFoundError as f:
-        print(f)
+    g = GED_Repo(os.getcwd())
+    g.print_individuals()
+    g.print_families()
 
 if __name__ == '__main__':
     main()
