4845761f710e3034525a769ee9eee822e6fc7412
Akshay Lavhagale <akshay.lavhagale27@gmail.com>
2020-03-02 14:39:25 -0500

    Add files via upload

---
+++ test_directory/US06/US06_Test.py
@@ -0,0 +1,29 @@
+"""
+User Story 05 (US05) - Test File
+US08: Birth before marriage of parents
+@Author: Akshay Lavhagale
+"""
+
+import unittest, os, io, sys
+from ssw555a_ged import GED_Repo
+
+
+class Test_US05(unittest.TestCase):
+    """ Tests that the user_story_5 function throws when expected. """
+
+    def test_user_story_5(self):
+        """ Tests that user_story_5 rejects illegitimate marriage by throwing a ValueError. """
+
+        # need following cases:
+        # divorce before death
+        g = GED_Repo(os.path.join(os.getcwd(), 'test_directory', 'US05', 'ssw555a_input.ged'))
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        g.check_bday()
+        sys.stdout = sys.__stdout__
+        output_str1 = 'Olivia /Mann/ divorce after individual death date on line 183\n'
+        self.assertEqual(capturedOutput.getvalue(), output_str1)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
