c6645cc74ad1660c4fc231cac7543a79398d9639
Zephyr Zambrano <43213451+zephyrzambrano@users.noreply.github.com>
2020-03-21 14:34:05 -0400

    Fixed typo

--- test_directory/US39/US39_Test.py
+++ test_directory/US39/US39_Test.py
@@ -20,15 +20,15 @@ class Test_US39(unittest.TestCase):
         """ Tests the methods in US39_upcoming_anniversaries """
         # upcoming anniversary
         g = GED_Repo([os.path.join(os.getcwd(), "test_directory", "US39", "US39_Upcoming_Anniversaries.ged")])
-        self.assertEqual(g.US38_print_upcoming_anniversaries, [('04/18/1990', 'Husband: Father /Lastname/', 'Wife: Mother /Oldlastname/')])
+        self.assertEqual(g.US39_print_upcoming_anniversaries, [('04/18/1990', 'Husband: Father /Lastname/', 'Wife: Mother /Oldlastname/')])
 
         # no upcoming anniversary
         g = GED_Repo([os.path.join(os.getcwd(), "test_directory", "US39", "US39_No_Upcoming_Anniversaries.ged")])
-        self.assertEqual(g.US38_print_upcoming_anniversaries, "No upcoming anniversaries.")
+        self.assertEqual(g.US39_print_upcoming_anniversaries, "No upcoming anniversaries.")
 
         # upcoming anniversary, but person is dead so it does not count for this program
         g = GED_Repo([os.path.join(os.getcwd(), "test_directory", "US39", "US39_Dead_Upcoming_Anniversaries.ged")])
-        self.assertEqual(g.US38_print_upcoming_anniversaries, "No upcoming anniversaries.")
+        self.assertEqual(g.US39_print_upcoming_anniversaries, "No upcoming anniversaries.")
 
 
 if __name__ == "__main__":
