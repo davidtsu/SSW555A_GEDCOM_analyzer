3b2d81b63ceb198655c048868c10a854775e479c
ekocibelli <54696646+ekocibelli@users.noreply.github.com>
2020-03-23 14:05:48 -0400

    Last fixes and changes to US21 and US22

--- test_directory/US22/US22_Test.py
+++ test_directory/US22/US22_Test.py
@@ -11,7 +11,7 @@ class Test_US22(unittest.TestCase):
     """ Tests that add_individual and add_family makes sure to have only unique ids. """
 
     def test_user_story_22(self):
-        """ Tests that add_family and add_individual prints out the duplicates. """
+        """ Tests that functions prints out if there is a duplicate. """
         # husband gender female and wife gender male (failure)
         g = GED_Repo([os.path.join(os.getcwd(), 'test_directory', 'US22', 'unique_ids_test.ged')])
         capturedOutput = io.StringIO()
@@ -24,14 +24,8 @@ class Test_US22(unittest.TestCase):
         sys.stdout = sys.__stdout__
         
         output_str1 = 'US22 - @I1-US22-A@ id has a duplicate in line number 12\n'
-        
-        # output_str1 = ['US22 - @I1-US22-A@ id has a duplicate in line number 12\n', 'US22 - @I3-US22-A@ id has a duplicate in line number 21\n', 'US22 - @I3-US22-A@ id has a duplicate in line number 21',
-        #         'US22 - @I4-US22-A@ id has a duplicate in line number 30\n', 'US22 - @I5-US22-A@ id has a duplicate in line number 40\n',
-        #         'US22 - @I6-US22-A@ id has a duplicate in line number 49\n']
 
-        
         self.assertEqual(capturedOutput.getvalue(), output_str1)
-
 
 if __name__ == "__main__":
     unittest.main(exit=False)
