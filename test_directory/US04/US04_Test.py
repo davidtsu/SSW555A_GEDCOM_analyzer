d1917d0875a98c5bed5a5dcb73a3be57d7218c02
Akshay Lavhagale <akshay.lavhagale27@gmail.com>
2020-03-23 15:44:46 -0400

    Update US04_Test.py

--- test_directory/US04/US04_Test.py
+++ test_directory/US04/US04_Test.py
@@ -3,26 +3,22 @@ User Story 04 (US04) - Test File
 US04: Marriage before divorce
 @Author: Akshay Lavhagale
 """
-
 import unittest, os, io, sys
 from ssw555a_ged import GED_Repo, Individual, Family
 
-
 class Test_US04(unittest.TestCase):
-    """ Tests that the user_story_4 function throws when expected. """
+    """ Marriage should occur before divorce of spouses, and divorce can only occur after marriage. """
 
     def test_user_story_4(self):
-        """ Tests that user_story_4 rejects illegitimate marriage by throwing a ValueError. """
-
-        # need following cases:
-        # Marriage before divorce
-        g = GED_Repo(os.path.join(os.getcwd(), 'test_directory', 'US04', 'ssw555a_input.ged'))
+        """ Tests that Marriage should occur before divorce of spouses, and divorce can only occur after marriage and rejects illegitimate data """
+        g = GED_Repo([os.path.join(os.getcwd(), 'test_directory', 'US04', 'US04_marriage_before_divorce.ged')])
         capturedOutput = io.StringIO()
         sys.stdout = capturedOutput
         g.user_story_4()
         sys.stdout = sys.__stdout__
         output_str1 = 'US04 - Jodie /John/ and Jimmy /John/ married after divorce on line 22\n'
         self.assertEqual(capturedOutput.getvalue(), output_str1)
+
 
 if __name__ == "__main__":
     unittest.main(exit=False)
